d974b2006385728ae834f8d40a75f56c0a7383cf
Tom <tom@contextfree.info>
2021-06-21 05:36:22 -0700

    Start

---
+++ Multi.java
@@ -0,0 +1,14 @@
+class Multi {
+    public static void main(String[] args) {
+        var text = """
+            Parameters
+            ----------
+            a : array_like
+                The shape and data-type of `a` define these same attributes of
+                the returned array.
+            dtype : data-type, optional
+                Overrides the data type of the result.
+        """;
+        System.out.println(text);
+    }
+}
---
+++ multi.py
@@ -0,0 +1,2 @@
+# https://github.com/numpy/numpy/blob/31e232c47e6be5ddc1c2433ccb97cec1640df012/numpy/core/numeric.py#L76
+
