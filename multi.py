00d2e5483e02a9f4898f30daf7348ea1d54a4117
Tom <tom@contextfree.info>
2021-06-21 06:03:19 -0700

    Add gif comment

--- multi.py
+++ multi.py
@@ -1,2 +1,6 @@
 # https://github.com/numpy/numpy/blob/31e232c47e6be5ddc1c2433ccb97cec1640df012/numpy/core/numeric.py#L76
 
+# http://probablyprogramming.com/2009/03/15/the-tiniest-gif-ever
+# In [1]: import base64
+# In [2]: base64.b64decode('R0lGODlhAQABAIABAP///wAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==')
+# Out[2]: b'GIF89a\x01\x00\x01\x00\x80\x01\x00\xff\xff\xff\x00\x00\x00!\xf9\x04\x01\n\x00\x01\x00,\x00\x00\x00\x00\x01\x00\x01\x00\x00\x02\x02L\x01\x00;'
