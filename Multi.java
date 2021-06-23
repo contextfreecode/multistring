36a9eb9697cd56913ccbc08b5c71467deed307d1
Tom <tom@contextfree.info>
2021-06-22 21:34:37 -0700

    Change text; work python

--- Multi.java
+++ Multi.java
@@ -4,28 +4,32 @@ class Multi {
      *             gives an array of just one element: "hi".
      */
     public static void main(String[] args) throws Exception {
+        // Example from https://openjdk.java.net/jeps/378
         var text = """
-            Parameters
-            ----------
-            a : array_like
-                The shape and data-type of `a` define these same attributes of
-                the returned array.
-            dtype : data-type, optional
-                Overrides the data type of the result.
+            function hello() {
+                print("name\tage");
+            }
+
+            hello();
         """;
         System.out.println(text);
-        var data =
-            "\u4749\u4638\u3961\u0100\u0100\u8001\u00ff\uffff\u0000\u0021" +
-            "\uf904\u010a\u0001\u002c\u0000\u0000\u0100\u0100\u0002\u024c" +
-            "\u0100\u3b00";
-        for (var c: data.toCharArray()) {
-            System.out.printf("%02x\n", c >> 8);
-            System.out.printf("%02x\n", c & 0xFF);
-        }
-        var d2 = Multi.class.getResourceAsStream("pixel.png").readAllBytes();
-        for (var b: d2) {
-            System.out.printf("%02x\n", b);
-        }
+        // var data =
+        //     "\u4749\u4638\u3961\u0100\u0100\u8001\u00ff\uffff\u0000\u0021" +
+        //     "\uf904\u010a\u0001\u002c\u0000\u0000\u0100\u0100\u0002\u024c" +
+        //     "\u0100\u3b00";
+        // // var data = """
+        // //     \u4749\u4638\u3961\u0100\u0100\u8001\u00ff\uffff\u0000\u0021\
+        // //     \uf904\u010a\u0001\u002c\u0000\u0000\u0100\u0100\u0002\u024c\
+        // //     \u0100\u3b00""";
+        // for (var c: data.toCharArray()) {
+        //     System.out.printf("%02x\n", c >> 8);
+        //     System.out.printf("%02x\n", c & 0xFF);
+        // }
+        // System.out.println("---");
+        // var d2 = Multi.class.getResourceAsStream("pixel.png").readAllBytes();
+        // for (var b: d2) {
+        //     System.out.printf("%02x\n", b);
+        // }
         // var data =
         //     "GIF89a\x01\x00\x01\x00\x80\x01\x00\xff\xff\xff\x00\x00\x00!" +
         //     "\xf9\x04\x01\n\x00\x01\x00,\x00\x00\x00\x00\x01\x00\x01\x00" +
--- multi.py
+++ multi.py
@@ -1,10 +1,38 @@
+def main():
+    text = """
+        function hello() {
+            print("name\tage");   
+        }
+
+        hello();
+    """
+    print(text)
+    data = (
+        "GIF89a\x01\x00\x01\x00\x80\x01\x00\xff\xff\xff\x00\x00\x00!"
+        "\xf9\x04\x01\n\x00\x01\x00,\x00\x00\x00\x00\x01\x00\x01\x00"
+        "\x00\x02\x02L\x01\x00;"
+    )
+    print(len(data))
+
+
+main()
+
+
 # https://github.com/numpy/numpy/blob/31e232c47e6be5ddc1c2433ccb97cec1640df012/numpy/core/numeric.py#L76
+# Parameters
+# ----------
+# a : array_like
+#     The shape and data-type of `a` define these same attributes of
+#     the returned array.
+# dtype : data-type, optional
+#     Overrides the data type of the result.
+
 
 # http://probablyprogramming.com/2009/03/15/the-tiniest-gif-ever
 # In [1]: import base64
 # In [2]: base64.b64decode('R0lGODlhAQABAIABAP///wAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==')
 # Out[2]: b'GIF89a\x01\x00\x01\x00\x80\x01\x00\xff\xff\xff\x00\x00\x00!\xf9\x04\x01\n\x00\x01\x00,\x00\x00\x00\x00\x01\x00\x01\x00\x00\x02\x02L\x01\x00;'
-# In [10]: a = base64.b64decode('R0lGODlhAQABAIABAP///wAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==')        
+# In [10]: a = base64.b64decode('R0lGODlhAQABAIABAP///wAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==')
 # In [11]: ", ".join(f"0x{_:02x}" for _ in a)
 # Out[11]: '0x47, 0x49, 0x46, 0x38, 0x39, 0x61, 0x01, 0x00, 0x01, 0x00, 0x80, 0x01, 0x00, 0xff, 0xff, 0xff, 0x00, 0x00, 0x00, 0x21, 0xf9, 0x04, 0x01, 0x0a, 0x00, 0x01, 0x00, 0x2c, 0x00, 0x00, 0x00, 0x00, 0x01, 0x00, 0x01, 0x00, 0x00, 0x02, 0x02, 0x4c, 0x01, 0x00, 0x3b'
 # In [3]: with open("pixel.png", "wb") as out: out.write(a)
