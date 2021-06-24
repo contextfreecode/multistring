a68379db754dcab0b379bb082230cdfabaedf10d
Tom <tom@contextfree.info>
2021-06-24 06:12:53 -0700

    Finish zig maybe

--- multi.zig
+++ multi.zig
@@ -1,7 +1,7 @@
 const std = @import("std");
+const stdout = std.io.getStdOut().writer();
 
 pub fn main() !void {
-    const stdout = std.io.getStdOut().writer();
     // Text
     const text =
         \\function hello() {
@@ -11,4 +11,11 @@ pub fn main() !void {
         \\hello();
     ;
     try stdout.print("{}\n", .{text});
+    // Data
+    // const data =
+    //     "GIF89a\x01\x00\x01\x00\x80\x01\x00\xff\xff\xff\x00\x00\x00!" ++
+    //     "\xf9\x04\x01\n\x00\x01\x00,\x00\x00\x00\x00\x01\x00\x01\x00" ++
+    //     "\x00\x02\x02L\x01\x00;";
+    const data = @embedFile("pixel.png");
+    try stdout.print("{}\n", .{data.len});
 }
